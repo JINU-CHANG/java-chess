3c03617f40544c78dde0116c08d9afedea4dd7cc
jinwoo22 <jenny010925@naver.com>
2024-03-29 14:32:34 +0900

    refactor: 체스보드 출력할 때 DTO 사용하도록 변경

--- src/main/java/chess/view/OutputView.java
+++ src/main/java/chess/view/OutputView.java
@@ -1,39 +1,24 @@
 package chess.view;
 
-import chess.domain.piece.Piece;
-import chess.domain.position.Position;
-import chess.domain.position.Rank;
-import java.util.ArrayList;
+import chess.dto.ChessBoardDto;
 import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
 
 public class OutputView {
 
-    public void printChessBoard(final Map<Position, Piece> pieces) {
-        final List<List<Character>> board = sortByBoardOrder(pieces);
-        board.forEach(this::printChessRow);
+    public void printCommandMenu() {
+        System.out.println("""
+                > 체스 게임을 시작합니다.
+                > 게임 시작 : start
+                > 게임 종료 : end
+                > 게임 이동 : move source위치 target위치 - 예. move b2 b3""");
+    }
+
+    public void printChessBoard(final ChessBoardDto chessBoardDto) {
+        chessBoardDto.chessBoard().forEach(this::printChessRow);
     }
 
     private void printChessRow(final List<Character> row) {
         row.forEach(System.out::print);
         System.out.println();
-    }
-
-    private List<List<Character>> sortByBoardOrder(final Map<Position, Piece> pieces) {
-        final List<List<Character>> board = new ArrayList<>();
-        for (int i = 0; i < Rank.values().length; i++) {
-            board.add(new ArrayList<>(List.of('.', '.', '.', '.', '.', '.', '.', '.')));
-        }
-
-        for (Entry<Position, Piece> entry : pieces.entrySet()) {
-            final int fileIndex = entry.getKey().getFileIndex() - 1;
-            final int rankIndex = 7 - (entry.getKey().getRankIndex() - 1);
-
-            final List<Character> marks = board.get(rankIndex);
-            marks.set(fileIndex, ChessBoardMarker.getSymbol(entry.getValue()));
-        }
-
-        return board;
     }
 }
